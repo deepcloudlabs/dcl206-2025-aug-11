c33996f6246172475ba995fe21eaf2c651d754c0
Binnur KURT <deepcloudlabs@gmail.com>
2025-08-15 17:46:56 +0300

    Update Exercise09.java

--- memory.leak.and.other.issues/src/com/example/test/Exercise09.java
+++ memory.leak.and.other.issues/src/com/example/test/Exercise09.java
@@ -6,7 +6,7 @@ import java.util.HashSet;
 public class Exercise09 {
 
 	public static void main(String[] args) {
-		var map = new HashSet<>(133_333,0.75f);
+		var map = new HashSet<>(200_000,0.5f);
 
 	}
 
